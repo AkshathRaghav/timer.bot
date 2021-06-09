4ec364d368b06a81759a4b4b11c69a4a6a75acab
Akshath Raghav <akshathraghav.r@gmail.com>
2021-06-10 05:17:52 +0530

    Create Bot.java

---
+++ src/Bot.java
@@ -0,0 +1,17 @@
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
+import javax.security.auth.login.LoginException;
+
+public class Bot {
+    public static JDA jda ;
+    public final static String prefix = ":t" ;
+    public static void main(String[] args) throws LoginException {
+        jda = JDABuilder.createDefault("<Insert token here>") .build();
+        jda.getPresence().setStatus(OnlineStatus.DO_NOT_DISTURB);
+        jda.getPresence().setActivity(Activity.playing("with time for you"));
+        jda.addEventListener(new Commands());
+
+    }
+}
