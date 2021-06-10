b26694b7c1e9b867fbde4aff710392478cb546d1
Akshath Raghav <akshathraghav.r@gmail.com>
2021-06-10 05:31:38 +0530

    Update Bot.java

--- src/Bot.java
+++ src/Bot.java
@@ -8,7 +8,7 @@ public class Bot {
     public static JDA jda ;
     public final static String prefix = ":c" ;
     public static void main(String[] args) throws LoginException {
-        jda = JDABuilder.createDefault("<Insert token here>") .build();
+        jda = JDABuilder.createDefault("<Insert token here>").build();
         jda.getPresence().setStatus(OnlineStatus.DO_NOT_DISTURB);
         jda.getPresence().setActivity(Activity.playing("with time for you"));
         jda.addEventListener(new Commands());
